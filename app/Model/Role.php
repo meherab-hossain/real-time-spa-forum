7b6c03c907b2a387d298ad87e90d7db5360b2d58
meherab-hossain <h.meherabhossain@gmail.com>
2019-10-23 22:23:12 +0600

    relationships

--- app/Model/Role.php
+++ app/Model/Role.php
@@ -2,9 +2,12 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    public function user(){
+        return $this->hasMany(User::class);
+    }
 }
